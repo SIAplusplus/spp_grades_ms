cb224f2b8785c60798d60cde72a77912c1dd538c
xabelian <juagarciama@unal.edu.co>
2022-10-16 21:48:47 -0500

    queue working

--- Grades/apps.py
+++ Grades/apps.py
@@ -1,4 +1,10 @@
 from django.apps import AppConfig
+from django_rabbitmq.mq import RabbitMQ
+
 
 class GradesConfig(AppConfig):
     name = 'Grades'
+    
+    def ready(self):
+        from Grades.mq import CustomRabbit
+        RabbitMQ.callback = CustomRabbit.callback
